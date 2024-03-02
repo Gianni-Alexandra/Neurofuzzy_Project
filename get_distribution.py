8729654f49de752278d57f3ced87245f29d8fe3f
Nikos Stylianou <stylianou404@gmail.com>
2024-03-02 21:01:53 +0100

    Report: Added final distribution class

--- get_distribution.py
+++ get_distribution.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib.ticker as StrLimit
+import matplotlib
 
 from imblearn.over_sampling import RandomOverSampler
 from imblearn.over_sampling import SMOTE
@@ -29,32 +30,36 @@ def clean (text):
 
 texts = texts.apply(clean)
 
-print(texts.head())
+# print('Some insights about the dataset: ')
+# print(df.describe()) 
 
-
-print('Some insights about the dataset: ')
-print(df.describe()) 
 plt.figure(figsize=(10, 5))
 sns.countplot(x='category_level_1', data=df)
 labels = [label[:10] for label in df['category_level_1'].unique()]  # Limit the length of labels to 10
 plt.xticks(ticks=range(len(labels)), labels=labels, rotation=45)  # Set the xticks with the limited labels
 plt.grid()
 plt.plot()
+plt.tight_layout()
 plt.show()
 
 # min_count = df['category_level_1'].value_counts().min()
 texts = texts.values.reshape(-1, 1)
-ros = SMOTE(random_state=777)
+ros = RandomOverSampler(random_state=777)
 X_ROS, Y_ROS = ros.fit_resample(texts, labels)
 
 X_ROS = pd.DataFrame(X_ROS)
 # X_ROS = pd.DataFrame(X_ROS, columns=['content'])
 
+print(X_ROS.describe())
+
 plt.figure(figsize=(10, 5))
 sns.countplot(x=Y_ROS)
 plt.title('Distribution of the categories after resampling')
-plt.xticks(rotation=45)
+# plt.xticks(rotation=45)
 # Optionally, you can also adjust the alignment and the spacing
+labels = [label[:10] for label in df['category_level_1'].unique()]  # Limit the length of labels to 10
+plt.xticks(ticks=range(len(labels)), labels=labels, rotation=45)  # Set the xticks with the limited labels
 plt.tight_layout()
+plt.grid()
 plt.plot()
 plt.show()
